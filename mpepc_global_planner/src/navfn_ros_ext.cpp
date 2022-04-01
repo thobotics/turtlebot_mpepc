86c8b2ecaf59ab1f4a995e52250489418cce1bd9
angcx1997 <ang.chin.xian@gmail.com>
2022-04-01 10:58:38 +0800

    Format with clang

--- mpepc_global_planner/src/navfn_ros_ext.cpp
+++ mpepc_global_planner/src/navfn_ros_ext.cpp
@@ -14,25 +14,26 @@
 #include <math.h>
 #include <algorithm>
 
-//register this planner as a BaseGlobalPlanner plugin
+// register this planner as a BaseGlobalPlanner plugin
 PLUGINLIB_EXPORT_CLASS(navfn::NavfnROSExt, nav_core::BaseGlobalPlanner)
 
-namespace navfn {
+namespace navfn
+{
 
-	static const double PI= 3.1415926535897932384626433832795028841971693993751058209749445923078164062862089986280348;
+	static const double PI = 3.1415926535897932384626433832795028841971693993751058209749445923078164062862089986280348;
 
-	NavfnROSExt::NavfnROSExt() :
-		global_planner::GlobalPlanner(){}
-	NavfnROSExt::NavfnROSExt(std::string name, costmap_2d::Costmap2D* costmap_ros, std::string frame_id) :
-		global_planner::GlobalPlanner(name, costmap_ros, frame_id){}
+	NavfnROSExt::NavfnROSExt() : global_planner::GlobalPlanner() {}
+	NavfnROSExt::NavfnROSExt(std::string name, costmap_2d::Costmap2D *costmap_ros, std::string frame_id) : global_planner::GlobalPlanner(name, costmap_ros, frame_id) {}
 
-	void NavfnROSExt::initialize(std::string name, costmap_2d::Costmap2DROS* costmap_ros){
+	void NavfnROSExt::initialize(std::string name, costmap_2d::Costmap2DROS *costmap_ros)
+	{
 		global_planner::GlobalPlanner::initialize(name, costmap_ros);
 		ROS_INFO("Initialized NavfnROSExt");
 	}
 
-	bool NavfnROSExt::makePlan(const geometry_msgs::PoseStamped& start,
-				  const geometry_msgs::PoseStamped& goal, std::vector<geometry_msgs::PoseStamped>& plan){
+	bool NavfnROSExt::makePlan(const geometry_msgs::PoseStamped &start,
+							   const geometry_msgs::PoseStamped &goal, std::vector<geometry_msgs::PoseStamped> &plan)
+	{
 		bool result = global_planner::GlobalPlanner::makePlan(goal, start, plan);
 
 		// Now copy orientation of goal pose to plan
@@ -44,4 +45,3 @@ namespace navfn {
 		return result;
 	}
 }
-
